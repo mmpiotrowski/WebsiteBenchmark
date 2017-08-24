a562c765ab11917145620e525205e5722f2af974
Michal <Michal@Michal-Laptop>
2017-08-24 12:40:35 +0200

    Change email body template

--- src/Callbacks/EmailCallbacks.php
+++ src/Callbacks/EmailCallbacks.php
@@ -25,7 +25,9 @@
  */
 
 namespace WebsiteBenchmark\Callbacks;
+
 use WebsiteBenchmark\Email;
+
 /**
  * EmailCallbacks class - benchmark callbacks 
  * send email with results after benchmark tests
@@ -33,53 +35,59 @@ use WebsiteBenchmark\Email;
  * @author Michal Piotrowski
  */
 class EmailCallbacks extends BenchmarkCallbacks {
-   
+
     protected $_email;
-   
+
     /**
      * Create Email object
      *     
      * @param \WebsiteBenchmark\Configure $config - need config to read email settings
      */
     public function __construct($benchmark) {
-       parent::__construct($benchmark);
-       
-       $host = $this->_config->get('email', 'host');
-       $port = $this->_config->get('email', 'port');
-       $encryption = $this->_config->get('email', 'encryption');
-       $username = $this->_config->get('email', 'username');
-       $password = $this->_config->get('email', 'password');       
-       $this->_email = new Email($host,$port,$encryption,$username,$password);
+        parent::__construct($benchmark);
+
+        $host = $this->_config->get('email', 'host');
+        $port = $this->_config->get('email', 'port');
+        $encryption = $this->_config->get('email', 'encryption');
+        $username = $this->_config->get('email', 'username');
+        $password = $this->_config->get('email', 'password');
+        $this->_email = new Email($host, $port, $encryption, $username, $password);
     }
-    
+
     /**
      * 
      * 
      * @param \WebsiteBenchmark\Ranking $ranking - ranking object contain benchmark results
      */
     public function afterRun($ranking) {
-      $from = $this->_config->get('email', 'from');   
-      $to = $this->_config->get('email', 'to'); 
-      $subject = 'Resutls for Website Benchmark';
-      $body =  '';
-      $rankingList = $ranking->getRanking();
+        $from = $this->_config->get('email', 'from');
+        $to = $this->_config->get('email', 'to');
+        $subject = 'Resutls for Website Benchmark';
+        $body = '';
+        
+        $body .= 'Benchmark date: ' . $ranking->getTestDate() . '<br>';
+        $body .= 'Benchmark subject: ' . $ranking->getSubject()->getName() . '<br>';
+        $body .= 'Ratio relative to the fastest: ' . $ranking->getSubjectFastestRatio() . '<br>';
+        $body .= 'Ratio relatie to the slowest : ' . $ranking->getSubjectSlowestRatio() . '<br>';
+
+        $rankingList = $ranking->getRanking();
         foreach ($rankingList as $p => $competitor) {
-            $position = $p +1;
+            $position = $p + 1;
             if ($competitor->isSubject()) {
-                $body .= $position.'. <span style="font-weight: bold; text-color:red;"> Results for ' . $competitor->getName() . ' time: ' . $competitor->getTestTime() .' ms </span>  <b> - this is benchmark subject </b>'. '<br>';
+                $body .= $position . '. <span style="font-weight: bold; text-color:red;"> Results for ' . $competitor->getName() . ' time: ' . $competitor->getTestTime() . ' ms </span>  <b> - this is benchmark subject </b>' . '<br>';
             } else {
                 $diffTime = $competitor->getDiffTime();
                 $after = 'slower';
-                if($diffTime < 0) $after = 'faster';
-                $body .= $position.'. Results for ' . $competitor->getName() . ' time: ' . $competitor->getTestTime() . ' ms ' . $diffTime.' ms '.$after . ' than the subject. <br>';
+                if ($diffTime < 0)
+                    $after = 'faster';
+                $body .= $position . '. Results for ' . $competitor->getName() . ' time: ' . $competitor->getTestTime() . ' ms ' . $diffTime . ' ms ' . $after . ' than the subject. <br>';
             }
         }
         $results = false;
-        if($ranking->getSubjectFastestRatio() > 2)
-            $results = $this->_email->send($from,$to,$subject,$body,'',TRUE);
-        
+        if ($ranking->getSubjectFastestRatio() > 2)
+            $results = $this->_email->send($from, $to, $subject, $body, '', TRUE);
+
         return $results;
-        
-        
     }
+
 }
